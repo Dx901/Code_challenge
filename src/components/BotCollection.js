9af5c063685ae6cd3e60fbf6671a0939ec0e839a
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-02 19:49:21 +0300

    2

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function BotCollection() {
+const [bots, setBots] = useState()
+
+ //function to collect the bots
+ const getBotData = async() => {
+  const res = await fetch(
+    "http://localhost:8002/bots"
+  ).then((res) => res.json())
+  setBots(res)
+ }
+
+ useEffect(() => {
+  getBotData()
+ }, [])
+
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
+        {bots &&
+        bots.map((bot) => (
+          <div className="bot-container">
+            id:{bot.id}
+            
+          </div>
+        ))}
         {/*...and here..*/}
         Collection of all bots
       </div>
@@ -13,3 +34,7 @@ function BotCollection() {
 }
 
 export default BotCollection;
+
+
+// return this.props.bots.map(bot => {
+//   return <BotCard key={bot.id} bot={bot} addBot={this.props.addBot}/>
